835ba3d6113db2b36c2a6de733230ecca2f1cc06
Daniel <danielcoellovargas@gmail.com>
2024-03-06 16:10:59 -0500

    Featured post - For-If

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-from blogs.models import Category
+from blogs.models import Blog, Category
 
 
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True)
+    
     context = {
         'categories': categories,
+        'featured_post': featured_posts,
     }
     return render(request, 'home.html', context)
