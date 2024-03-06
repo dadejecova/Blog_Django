ab9133090d23de4bf10820859b4f596fd171efca
Daniel <danielcoellovargas@gmail.com>
2024-03-06 16:39:38 -0500

    Display Featured Posts on Homepage

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,10 +5,10 @@ from blogs.models import Blog, Category
 
 def home(request):
     categories = Category.objects.all()
-    featured_posts = Blog.objects.filter(is_featured=True)
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('-updated_at')
     
     context = {
         'categories': categories,
-        'featured_post': featured_posts,
+        'featured_posts': featured_posts,
     }
     return render(request, 'home.html', context)
